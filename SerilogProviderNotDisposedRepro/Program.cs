81d3543e79e26f46d2109dc0a4edc55aa6b16ca4
Aleksey S <aleksey.s902@gmail.com>
2021-01-28 18:27:09 +0200

    Initial commit

---
+++ SerilogProviderNotDisposedRepro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogProviderNotDisposedRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    builder.AddFile("test.log");
+                })
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogError("test");
+            
+            serviceProvider.Dispose();
+            // Thread.Sleep(5000);
+        }
+    }
+}
